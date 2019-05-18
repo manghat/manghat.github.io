64c9e41cf0f8258b729809893af7608b65a534f2
Ashwin Manghat <ashwinmanghat@gmail.com>
2019-05-19 01:09:00 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by Ashwin Manghat',
+  subtitle: 'Learnings, musings and sounding baord of a life long learner ',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'Photography',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Ashwin Manghat',
+    photo: '/photo.jpg',
+    bio: 'Photography. Code. Learn.',
+    contacts: {
+      email: 'ashwinmanghat@gmail.com',
+      // telegram: '#',
+      twitter: 'manghat',
+      github: 'manghat',
+      // rss: '#',
+      // vkontakte: '#'
+    }
+  }
+};
